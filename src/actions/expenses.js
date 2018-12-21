347275c85993cd3d79751e96ae4b75dc3fd54eaf
Royfangs <royfangs@gmail.com>
2018-12-21 16:58:33 +0800

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -33,6 +33,14 @@ export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
   id
 });
+
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({ id }));
+    });
+  };
+};
   
 //Edit expense
 
