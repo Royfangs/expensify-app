8b4144b14afda615a0baa9aa1f01b790b5c55f4d
Royfangs <royfangs@gmail.com>
2020-08-02 11:26:11 +0800

    Delete unused folder.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,22 @@ import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
 export const Header = ({ startLogout }) => (
-  <header className="header">
-    <div className="content-container">
-      <div className="header__content">
-        <Link className="header__title" to="/dashboard" >
-          <h1>Expensify</h1>
+  <header className='header'>
+    <div className='content-container'>
+      <div className='header__content'>
+        <Link className='header__title' to='/dashboard'>
+          <h1>ExpenseRecord</h1>
         </Link>
-        <button className="button button--link" onClick={startLogout}>Logout</button>
+        <button className='button button--link' onClick={startLogout}>
+          Logout
+        </button>
       </div>
     </div>
   </header>
 );
 
 const mapDispatchToProps = (dispatch) => ({
-  startLogout: () => dispatch(startLogout())
+  startLogout: () => dispatch(startLogout()),
 });
 
 export default connect(undefined, mapDispatchToProps)(Header);
